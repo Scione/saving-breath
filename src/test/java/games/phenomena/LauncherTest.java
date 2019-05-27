1b7b805f5cb6761dfb2f28a0568c07754c34e0b9
Scione <scione@phenomena.games>
2019-05-27 21:58:31 +0200

    Add project structure

---
+++ src/test/java/games/phenomena/LauncherTest.java
@@ -0,0 +1,12 @@
+package games.phenomena;
+
+import org.junit.jupiter.api.Test;
+
+public class LauncherTest
+{
+    @Test
+    public void testLauncher()
+    {
+        Launcher.main(null);
+    }
+}
